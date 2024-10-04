80448bb392f25483182811d366e3367132296407
tarjeihe <tarjeihe@math.uio.no>
2024-10-04 13:56:26 +0200

    Update poisson2d.py

--- poisson2d.py
+++ poisson2d.py
@@ -197,8 +197,8 @@ def test_interpolation():
     assert abs(sol.eval(sol.h/2, 1-sol.h/2) - ue.subs({x: sol.h/2, y: 1-sol.h/2}).n()) < 1e-3
 
 if __name__ == '__main__':
-    test_convergence_poisson2d()
-    #test_interpolation()
+    #test_convergence_poisson2d()
+    test_interpolation()
     
 
 
